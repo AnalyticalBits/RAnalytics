6ad329b7f9f11dad9f21016280edf1327954234d
Bhavesh Patel <bxp08@LT-5CG7451RW3>
2018-06-11 14:25:12 +1000

    Programming Basics

--- ExcelToRScript.R
+++ ExcelToRScript.R
@@ -10,3 +10,24 @@ toupper("bhavesh")
 ## ggplot2 plotting
 library(ggplot2)
 qplot(x=mpg, y=wt, data =mtcars, colour = cyl)
+
+
+## R Programming Basics
+# Assigning Variables
+a =5
+b= 6
+Total = a+b
+Total
+## Conditions
+ # Equality : ==
+ # Not Equal: !=
+ # Greater/Less than: > or < 
+ # Greater/Less than or equal to : >= or <=
+
+# Does 5 equal 4?
+ 8 == 2
+# Is 10 less than or equal to 15
+ 10 <= 15
+# Does TRUE not equal TRUE?
+ TRUE !=TRUE
+ 
